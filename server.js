364a79287ed06dc555f7a92a5434f9b9b09fef39
Himanshu <hv308489@gmail.com>
2025-05-03 21:20:20 +0530

    server

--- server.js
+++ server.js
@@ -33,7 +33,7 @@ mongoose
   })
   .then(() => {
     console.log('MongoDB connected');
-    const PORT = process.env.PORT || 5000;
+    const PORT = process.env.PORT;
     server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
   })
   .catch((err) => console.error('MongoDB connection error:', err));
